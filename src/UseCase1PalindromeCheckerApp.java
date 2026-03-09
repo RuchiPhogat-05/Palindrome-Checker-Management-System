c0dfd0fc9d9afdd01f000c3b0f1c3846391ef14b
RuchiPhogat-05 <ruchiphogat1509@gmail.com>
2026-03-09 14:40:19 +0530

    Initial commit - Palindrome Checker App

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+public class UseCase1PalindromeCheckerApp {
+
+    // Application details
+    static String appName = "Palindrome Checker Management System";
+    static String appVersion = "Version 1.0";
+
+    public static void main(String[] args) {
+
+        // Step 1: Program starts
+        System.out.println("Program Started...");
+
+        // Step 2: Display Application Name
+        System.out.println("Application Name: " + appName);
+
+        // Step 3: Display Application Version
+        System.out.println("Application Version: " + appVersion);
+
+        // Step 4: Continue or Exit
+        System.out.println("Program continues to next use case or exits.");
+    }
+}
